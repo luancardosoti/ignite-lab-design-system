7d2e1dc92d1dd97ac7987cfccd517d327aee00aa
Luan Cardoso <luancardosoti@gmail.com>
2022-10-11 23:15:52 -0300

    construindo design system baseado no figma com storybook e talwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx'
+  ],
+  theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32
+    },
+    colors: {
+      transparent: 'transparent',
+
+      white: '#fff',
+      black: '#000',
+
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        400: '#7c7c7c',
+        200: '#c4c4cc',
+        100: '#e1e1e6'
+      },
+
+      cyan: {
+        300: '#9BE1FB',
+        500: '#81d8f7'
+      }
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
